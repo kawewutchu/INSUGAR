dc5381c0efab3da32dcc22145b078b9e31b3b431
kawewut <kawewut.chu@gmail.com>
2017-04-05 16:33:17 +0700

    f

---
+++ INSUGAR/BooldCell.swift
@@ -0,0 +1,24 @@
+//
+//  BooldCell.swift
+//  INSUGAR
+//
+//  Created by Kawewut Chujit on 3/24/2560 BE.
+//  Copyright © 2560 kritsada sangmoon. All rights reserved.
+//
+
+import UIKit
+
+class BooldCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ INSUGAR/Insurin.swift
@@ -0,0 +1,13 @@
+//
+//  Insurin.swift
+//  INSUGAR
+//
+//  Created by Kawewut Chujit on 3/24/2560 BE.
+//  Copyright © 2560 kritsada sangmoon. All rights reserved.
+//
+
+import UIKit
+
+class Insurin: NSObject {
+
+}
